a452b67e6d817da9b47cfac10da52f54ec020009
Abelardo Vieira Mota <abevieiramota@gmail.com>
2016-12-13 00:36:56 -0200

    aula 4 alura

---
+++ servlet/src/main/java/br/com/abevieiramota/servlet/web/FiltroAuditoria.java
@@ -0,0 +1,37 @@
+package br.com.abevieiramota.servlet.web;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+
+@WebFilter("/*")
+public class FiltroAuditoria implements Filter {
+
+	@Override
+	public void init(FilterConfig filterConfig) throws ServletException {
+	}
+
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+		
+		HttpServletRequest req = (HttpServletRequest) request;
+		String uri = req.getRequestURI();
+		
+		System.out.println("Usuário acessando " + uri);
+		
+		chain.doFilter(request, response);
+	}
+
+	@Override
+	public void destroy() {
+	}
+
+}
